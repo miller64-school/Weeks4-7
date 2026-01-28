703e37f84e5020810b14fbbe8c2948603c1eb179
mredraith <miller64@sheridancollege.ca>
2026-01-28 10:16:57 -0500

    Scripting practice

---
+++ Assets/Scripts/UIDemo.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class UIDemo : MonoBehaviour
+{
+    SpriteRenderer sr;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
