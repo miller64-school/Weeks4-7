5092d8098813c6fb64f63196341f7d58d496faa3
mredkcm <miller64@sheridancollege.ca>
2026-02-15 15:31:18 -0500

    Added startup sound. Rewrote zombie movement

--- Assets/Scripts/ZombieMove.cs
+++ Assets/Scripts/ZombieMove.cs
@@ -3,17 +3,30 @@ using UnityEngine;
 
 public class ZombieMove : MonoBehaviour
 {
-    public float speed = 3;
+    public float speed = 3f;
+    public float moveDirection;
 
     public void Start()
     {
-        InvokeRepeating("ZombieMove", 1, 1);
+        float startX = Random.value < 0.5f ? -13f : 13f;
+        transform.position = new Vector3(startX, transform.position.y, transform.position.z);
+
+        if (transform.position.x > 0.1f)
+        {
+            moveDirection = -1f;
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (transform.position.x < -0.1f)
+        {
+            moveDirection = 1f;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
 
     public void Update()
     {
-        Vector2 newPos = transform.position;
-        newPos.x += speed * Time.deltaTime;
-        transform.position += transform.right * speed * Time.deltaTime;
+        transform.position += Vector3.right * moveDirection * speed * Time.deltaTime;
+        if (transform.position.x > 14f || transform.position.x < -14f)
+            Destroy(gameObject);
     }
 }
